d4c057b7e5d57751434a864bda4bc51d05071150
riomulya <riomulya75@gmail.com>
2024-11-24 20:10:09 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,10 @@
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
-import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      srcDir: 'src',
-      filename: 'sw.js',
-      strategies: 'injectManifest', // Menggunakan file `sw-src.js` Anda
-      injectManifest: {
-        swSrc: './src/sw-src.js', // Lokasi file sumber Service Worker
-      },
-    }),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
