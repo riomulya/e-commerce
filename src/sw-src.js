149cbaa2c4f05eb5ca3431812cb022e19757e3c5
riomulya <riomulya75@gmail.com>
2024-11-24 20:57:34 +0700

    update

--- src/sw-src.js
+++ src/sw-src.js
@@ -5,33 +5,36 @@ import { ExpirationPlugin } from 'workbox-expiration';
 import { cacheNames } from 'workbox-core';
 
 precacheAndRoute(self.__WB_MANIFEST);
-let currentCacheNames = Object.assign({ precacheTemp: cacheNames.precache + "-temp" }, cacheNames);
+let currentCacheNames = Object.assign(
+  { precacheTemp: cacheNames.precache + '-temp' },
+  cacheNames
+);
 
-currentCacheNames.fonts = "googlefonts";
+currentCacheNames.fonts = 'googlefonts';
 registerRoute(
-	/https:\/\/fonts.(?:googleapis|gstatic).com\/(.*)/,
-	new CacheFirst({
-		cacheName: currentCacheNames.fonts,
-		plugins: [new ExpirationPlugin({ maxEntries: 30 })]
-	}),
-	"GET"
+  /https:\/\/fonts.(?:googleapis|gstatic).com\/(.*)/,
+  new CacheFirst({
+    cacheName: currentCacheNames.fonts,
+    plugins: [new ExpirationPlugin({ maxEntries: 30 })],
+  }),
+  'GET'
 );
 
 // clean up old SW caches
-self.addEventListener("activate", function (event) {
-	event.waitUntil(
-		caches.keys().then(function (cacheNames) {
-			let validCacheSet = new Set(Object.values(currentCacheNames));
-			return Promise.all(
-				cacheNames
-					.filter(function (cacheName) {
-						return !validCacheSet.has(cacheName);
-					})
-					.map(function (cacheName) {
-						console.log("deleting cache", cacheName);
-						return caches.delete(cacheName);
-					})
-			);
-		})
-	);
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      let validCacheSet = new Set(Object.values(currentCacheNames));
+      return Promise.all(
+        cacheNames
+          .filter(function (cacheName) {
+            return !validCacheSet.has(cacheName);
+          })
+          .map(function (cacheName) {
+            console.log('deleting cache', cacheName);
+            return caches.delete(cacheName);
+          })
+      );
+    })
+  );
 });
