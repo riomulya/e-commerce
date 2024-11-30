b34c6226346303c314f3deefe9e9360bca6165c1
Rio Mulya Syawal <riomulya75@gmail.com>
2024-11-30 10:48:01 +0700

    update

--- src/views/index.js
+++ src/views/index.js
@@ -8,7 +8,7 @@ export { default as ForgotPassword } from './auth/forgot_password';
 export { default as SignIn } from './auth/signin';
 export { default as SignUp } from './auth/signup';
 export { default as CheckOutStep1 } from './checkout/step1';
-export { default as CheckOutStep2 } from './checkout/step2';
+// export { default as CheckOutStep2 } from './checkout/step2';
 // export { default as CheckOutStep3 } from './checkout/step3';
 export { default as PageNotFound } from './error/PageNotFound';
 export { default as FeaturedProducts } from './featured';
